c202d9e2040ee67d7a9aec22822c2fdce5a9c986
pravin shelake <pravinshelake1997@gmail.com>
2024-02-29 11:07:13 +0530

    user related information edit

--- instakilo/user/urls.py
+++ instakilo/user/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from user.views import Login
+from user.views import Login , RegistrationView
 
 urlpatterns = [
-    path("login/",Login.as_view() )
+    path("login/",Login.as_view() ) ,
+    path("register/",RegistrationView.as_view())
 
 ]
--- instakilo/user/views.py
+++ instakilo/user/views.py
@@ -2,10 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from user.serializers import LoginSerializer
+from user.serializers import LoginSerializer, RegistrationSerializer
+
+
 
 
 class Login(APIView):
+    """This class create for user login purpose"""
 
     def post(self,request, format=None):
         serializers_class = LoginSerializer
@@ -16,12 +19,25 @@ class Login(APIView):
         )
     
 
-class Registration(APIView):
+class RegistrationView(APIView):
+    """this class id for the login page"""
 
-    def post(self,request, format=None):
-        serializers_class = RegistrationSerializer
-        serializers=serializers_class(data=request.data)
-        serializers.is_valid(raise_exception=True)
-        return Response(
-            data={"Success":True}
-        )
+    def post(self, request):
+        """post function for the class login
+
+        Args:
+            request (type): description
+            format (type, optional): description. Defaults to None.
+
+        Returns:
+            type: description
+        """
+
+        serializer_class = RegistrationSerializer
+        print(request.data)
+
+        register = serializer_class(data=request.data)
+        if register.is_valid(raise_exception=True):
+            print("valid data", register.validated_data)
+            return Response(
+                data={"success": True, "data": request.data}, status=status.HTTP_200_OK)
